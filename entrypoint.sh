24802610cb817884f18a2a3cc0e18e86df1811d5
Michal Jurečko <michal.jurecko@gmail.com>
2021-08-23 10:35:35 +0200

    Add missing component ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,15 @@ then
   exit 1
 fi
 export KBC_STORAGE_TOKEN=$1
-export TAG=$2
-CONFIGS=$3
+export COMPONENT_ID=$2
+export TAG=$3
+CONFIGS=$4
 
 docker pull -q quay.io/keboola/syrup-cli:latest
 TASK='
   echo "Running config {} with tag $TAG ..." &&
-  docker run --rm -e KBC_STORAGE_TOKEN quay.io/keboola/syrup-cli:latest run-job keboola.ex-db-mysql {} $TAG &&
+  docker run --rm -e KBC_STORAGE_TOKEN quay.io/keboola/syrup-cli:latest run-job $COMPONENT_ID {} $TAG &&
   echo "Config {} OK"
 '
-parallel --env TAG --lb -j 0 --halt now,fail=1 "$TASK" ::: $CONFIGS
+parallel --env COMPONENT_ID --env TAG --lb -j 0 --halt now,fail=1 "$TASK" ::: $CONFIGS
 echo "OK"
