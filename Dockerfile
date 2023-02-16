523b599c96063a2b7b4d6a3aec34a494f6d9e544
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-02-16 12:55:19 +0100

    add optional `host` and use KAC CLI

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM docker
 
-RUN apk add parallel
+RUN wget https://github.com/keboola/keboola-as-code/releases/download/v2.12.1/keboola-cli_2.12.1_linux_amd64.apk
+RUN apk add --allow-untrusted ./keboola-cli_2.12.1_linux_amd64.apk
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/sh -l
 set -e
 
-if [ $# -lt 3 ]
+if [ $# -lt 4 ]
 then
   echo "Some parameters are missing"
   exit 1
 fi
-export KBC_STORAGE_TOKEN=$1
-export COMPONENT_ID=$2
-export TAG=$3
-CONFIGS=$4
 
-docker pull -q quay.io/keboola/syrup-cli:latest
-TASK='
-  echo "Running config {} with tag $TAG ..." &&
-  docker run --rm -e KBC_STORAGE_TOKEN quay.io/keboola/syrup-cli:latest run-job $COMPONENT_ID {} $TAG &&
-  echo "Config {} OK"
-'
-parallel --env COMPONENT_ID --env TAG --lb -j 0 --halt now,fail=1 "$TASK" ::: $CONFIGS
-echo "OK"
+HOST=$1
+TOKEN=$2
+COMPONENT=$3
+TAG=$4
+for id in $5
+do
+  JOBS="${JOBS} $COMPONENT/$id@$TAG"
+done
+
+kbc remote job run --storage-api-host $HOST --storage-api-token $TOKEN $JOBS
